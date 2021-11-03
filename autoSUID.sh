c588e5ce2ede72b1353c972b3ff04c0f0e8a8fa0
alb3rtov <albertovmrm@gmail.com>
2021-11-03 09:31:00 +0100

    Add colors to output

--- autoSUID.sh
+++ autoSUID.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+#
 # Author: alb3rtov
+#
+
+# Colors ANSI escape codes
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
 
 # Global variables
 declare -a exploitable_binaries
+declare -a url_exploitable_binaries
 
 function ctrl_c() {
-	echo -e "\nCtrl+C signal caught...\n"
+	echo -e "\n${YELLOW}[*] Ctrl+C signal caught...\n${NC}"
+	tput cnorm
 	exit 1
 }
 
@@ -20,27 +35,29 @@ function search_binaries() {
 		
 		for binary in ${suid_binaries[@]}; do
 			if [[ $current_binary == $binary ]]; then
-				echo -e "[*] Match for $binary"
+				echo -e "\n${YELLOW}[*] SUID permissions match for ${NC}${LIGHTPURPLE}$binary${NC}"
 				exploitable_binaries+=($(which $binary))
+				url_exploitable_binaries+=($binary_url)
 			fi
 		done
 	done
 	echo -e ""
 	
 	if [[ ${#exploitable_binaries[@]} -eq 0 ]]; then
-		echo -e "[*] SUID binaries not found, exiting...\n"
+		echo -e "${YELLOW}[*] SUID binaries not found, exiting...\n${NC}"
+		tput cnorm
 		exit 0
 	fi
-	
-	#for element in ${exploitable_binaries[@]}; do
-	#	echo "$element"
-	#done
 }
 
 # Main function
+
+tput civis
 
 gtfo_url="https://gtfobins.github.io"
 
 declare -a suid_urls=$(curl -s $gtfo_url -X GET | grep "#suid" | sed 's/<li><a href="//' | sed 's/">SUID<\/a><\/li>//')
 
 search_binaries
+
+tput cnorm
